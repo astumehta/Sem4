90d54c113a11f333e1084d9ce34ef0f1e16da521
astumehta <143540555+astumehta@users.noreply.github.com>
2024-05-06 22:26:28 +0530

    Added codes

---
+++ DAA_EXPS/SumOfSubset.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define max 100
+int n, m;
+int w[max];
+int x[max];
+void sumOfSubsets(int s, int k, int r)
+{
+    int i;
+    x[k] = 1;
+    if (s + w[k] == m)
+    {
+        for (i = 0; i < n; i++)
+        {
+            printf("%d ", x[i]);
+        }
+        printf("\n");
+    }
+    else if (s + w[k] + w[k + 1] <= m)
+    {
+        sumOfSubsets(s + w[k], k + 1, r - w[k]);
+    }
+    if (s + w[k + 1] <= m && s + r - w[k] >= m)
+    {
+        x[k] = 0;
+        sumOfSubsets(s, k + 1, r - w[k]);
+    }
+}
+int main()
+{
+    int i;
+    int total = 0;
+    printf("Number of elements : ");
+    scanf("%d", &n);
+    printf("Enter element %d: ", i + 1);
+    for (i = 0; i < n; i++)
+    {
+
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &w[i]);
+        total = total + w[i];
+        x[i] = 0;
+    }
+    printf("Sum : ");
+    scanf("%d", &m);
+    sumOfSubsets(0, 0, total);
+}
---
+++ OS_exps/EXP6_bankers/Bankers.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+int allocation[5][3] = {
+    {0, 1, 0},
+    {2, 0, 0},
+    {3, 0, 2},
+    {2, 1, 1},
+    {0, 0, 2},
+};
+
+int max[5][3] = {
+    {7, 5, 3},
+    {3, 2, 2},
+    {9, 0, 2},
+    {2, 2, 2},
+    {4, 3, 3},
+};
+
+int visited[5] = {0};
+int need[5][3];
+int available[3] = {3, 3, 2};
+
+void calculateNeed()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            need[i][j] = max[i][j] - allocation[i][j];
+        }
+    }
+}
+
+void bankers()
+{
+    calculateNeed();
+
+    int work[3]; 
+    for (int i = 0; i < 3; i++)
+    {
+        work[i] = available[i];
+    }
+
+    int safeSequence[5];
+    int count = 0;
+
+    while (count < 5)
+    {
+        int found = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            if (!visited[i])
+            {
+                int canfinish = 1;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (need[i][j] > work[j])
+                    {
+                        canfinish = 0;
+                        break;
+                    }
+                }
+
+                
+                if (canfinish)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        work[k] += allocation[i][k];
+                    }
+                    visited[i] = 1;
+                    safeSequence[count++] = i;
+                    found = 1;
+                    break;
+                }
+            }
+        }
+        if (!found)
+        {
+            printf("No safe sequence\n");
+            return;
+        }
+    }
+
+    printf("Safe sequence found: ");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("P%d ", safeSequence[i] + 1);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    bankers();
+    return 0;
+}
