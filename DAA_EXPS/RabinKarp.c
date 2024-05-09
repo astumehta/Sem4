335c25c41abe83d6419b30205817c0f67b42f5e6
astumehta <143540555+astumehta@users.noreply.github.com>
2024-05-09 17:51:52 +0530

    now

--- DAA_EXPS/RabinKarp.c
+++ DAA_EXPS/RabinKarp.c
@@ -8,21 +8,26 @@ void search(char pat[], char txt[], int q)
     int M = strlen(pat);
     int N = strlen(txt);
     int i, j;
+
+    int h = 1;
+
     int p = 0;
     int t = 0;
-    int h = 1;
+    // M-1
+    // M
+    // <=N-M
 
     for (i = 0; i < M - 1; i++)
     {
         h = (h * d) % q;
     }
 
+    // MAIN
     for (i = 0; i < M; i++)
     {
         p = (d * p + pat[i]) % q;
         t = (d * t + txt[i]) % q;
     }
-
     for (i = 0; i <= N - M; i++)
     {
         if (p == t)
@@ -39,6 +44,7 @@ void search(char pat[], char txt[], int q)
                 printf("Pattern found at index %d \n", i);
             }
         }
+        
         if (i < N - M)
         {
             t = (d * (t - txt[i] * h) + txt[i + M]) % q;
--- DAA_EXPS/SumOfSubset.c
+++ DAA_EXPS/SumOfSubset.c
@@ -1,49 +1,34 @@
-
-// EASY SAME AS ALGO EXACT 
-
 #include <stdio.h>
-#define max 100
-int n, m;
-int w[max];
-int x[max];
-void sumOfSubsets(int s, int k, int r)
+int s = 0;
+int k = 0;
+int r = 73;
+int w[100] = {5, 10, 12, 13, 15, 18};
+int m = 30;
+int x[100];
+int n = 6;
+int SumOfSubsets(int s, int k, int r)
 {
-    int i;
     x[k] = 1;
     if (s + w[k] == m)
     {
-        for (i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            printf("%d ", x[i]);
+            printf("%d", x[i]);
         }
         printf("\n");
     }
     else if (s + w[k] + w[k + 1] <= m)
     {
-        sumOfSubsets(s + w[k], k + 1, r - w[k]);
+        SumOfSubsets(s + w[k], k + 1, r - w[k]);
     }
-    if (s + w[k + 1] <= m && s + r - w[k] >= m)
+    if (s + r - w[k] >= m && s + w[k + 1] <= m)
     {
         x[k] = 0;
-        sumOfSubsets(s, k + 1, r - w[k]);
+        SumOfSubsets(s, k + 1, r - w[k]);
     }
+    x[k] = 0;
 }
 int main()
 {
-    int i;
-    int total = 0;
-    printf("Number of elements : ");
-    scanf("%d", &n);
-    printf("Enter element %d: ", i + 1);
-    for (i = 0; i < n; i++)
-    {
-
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &w[i]);
-        total = total + w[i];
-        x[i] = 0;
-    }
-    printf("Sum : ");
-    scanf("%d", &m);
-    sumOfSubsets(0, 0, total);
+    SumOfSubsets(s, k, r);
 }
