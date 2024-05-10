ba48844194daf39e5568f9fb617d5278f8cfc501
astumehta <143540555+astumehta@users.noreply.github.com>
2024-05-10 15:11:34 +0530

    now

--- POA_EXPS/EXP4_pagereplacement/Optimal.c
+++ POA_EXPS/EXP4_pagereplacement/Optimal.c
@@ -1,87 +1,72 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-#define SIZE 4
+void optimal(int arr[], int size, int capacity) {
+    int miss = 0;
+    int hit = 0;
+    int sol_arr[capacity];
+    int index[capacity];
 
-int frames[SIZE];                                      // Frames in memory
-int pages[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2}; // Reference string
-int page_count = sizeof(pages) / sizeof(pages[0]);
-bool isPresent(int page)
-{
-    for (int i = 0; i < SIZE; i++)
-    {
-        if (frames[i] == page)
-        {
-            return true;
+    for (int i = 0; i < size; i++) {
+        if (i < capacity) {
+            sol_arr[i] = arr[i];
+            index[i] = i + 1;
+            miss++;
+            printf("Step %d: Element %d inserted into memory at index %d (Miss)\n", (i + 1), arr[i], i);
+        } else {
+            int found = 0;
+            for (int j = 0; j < capacity; j++) {
+                // Check if the page is already in memory
+                if (sol_arr[j] == arr[i]) {
+                    hit++;
+                    index[j] = i + 1; // Update index of current reference
+                    found = 1;
+                    printf("Step %d: Element %d found in memory at index %d (Hit)\n", (i + 1), arr[i], j);
+                    break;
+                }
+            }
+            if (!found) {
+                // Find the page to replace using Optimal Page Replacement Algorithm
+                int farthestIndex = -1;
+                int pageToReplace = -1;
+                for (int j = 0; j < capacity; j++) {
+                    int foundFutureReference = 0;
+                    for (int k = i + 1; k < size; k++) {
+                        if (sol_arr[j] == arr[k]) {
+                            index[j] = k + 1; // Update index of future reference
+                            foundFutureReference = 1;
+                            break;
+                        }
+                    }
+                    if (!foundFutureReference) {
+                        pageToReplace = j;
+                        break;
+                    } else {
+                        if (index[j] > farthestIndex) {
+                            farthestIndex = index[j];
+                            pageToReplace = j;
+                        }
+                    }
+                }
+
+                // Replace the page
+                sol_arr[pageToReplace] = arr[i];
+                index[pageToReplace] = i + 1;
+                miss++;
+                printf("Step %d: Element %d inserted into memory at index %d (Miss)\n", (i + 1), arr[i], pageToReplace);
+            }
         }
     }
-    return false;
+
+    printf("Total Misses: %d\n", miss);
+    printf("Total Hits: %d\n", hit);
 }
 
-void replacePage(int page, int *page_faults, int *index)
-{
-    int lru_index = 0;
-    for (int i = 1; i < SIZE; i++)
-    {
-        if (index[i] < index[lru_index])
-        {
-            lru_index = i;
-        }
-    }
-    frames[lru_index] = page;
-    (*page_faults)++;
-}
+int main() {
+    int arr[] = {7,0,1,2,0,3,0,4,2,3,0,3,2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int capacity = 4;
 
-void printFrames()
-{
-    for (int i = 0; i < SIZE; i++)
-    {
-        if (frames[i] == -1)
-        {
-            printf("-\t");
-        }
-        else
-        {
-            printf("%d\t", frames[i]);
-        }
-    }
-    printf("\n");
-}
-
-int main()
-{
-    int page_faults = 0;
-    int hits = 0;
-    int index[SIZE]; // Index to store the latest occurrence of pages in frames
-    for (int i = 0; i < SIZE; i++)
-    {
-        frames[i] = -1; // Initialize frames to -1
-        index[i] = 0;   // Initialize index to 0
-    }
-
-    printf("Frames:\n");
-    for (int i = 0; i < SIZE; i++)
-    {
-        printf("F%d\t", i);
-    }
-    printf("\n");
-
-    for (int i = 0; i < page_count; i++)
-    {
-        if (isPresent(pages[i]))
-        {
-            hits++;
-        }
-        else
-        {
-            replacePage(pages[i], &page_faults, index);
-        }
-        index[pages[i]] = i;
-        printFrames();
-    }
-
-    printf("Number of hits: %d\n", hits);
-    printf("Number of misses: %d\n", page_faults);
+    optimal(arr, size, capacity);
 
     return 0;
 }
