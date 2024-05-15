13b8ddf67d4ba801247f088b9bb2b131364c7448
astumehta <143540555+astumehta@users.noreply.github.com>
2024-05-15 20:14:17 +0530

    Corrected optimal code

--- POA_EXPS/EXP4_pagereplacement/Optimal.py
+++ POA_EXPS/EXP4_pagereplacement/Optimal.py
@@ -1,30 +1,38 @@
-def farthest_index(f,pages, startindex):
-        farthest_index = -1
-        farthest_dist = -1
-        for i, page in enumerate(f):
-            if page not in pages[startindex:]:
-                return i
-            else:
-                distance = pages[startindex:].index(page)
-                if distance > farthest_dist:
-                    farthest_dist = distance
-                    farthest_index = i
-        return farthest_index
+def index(f, pages, start):
+    index_val = -1
+    dist = -1
+    for i, page in enumerate(f):
+        if page not in pages[start:]:
+            return i
+        else:
+            distance = pages[start:].index(page)
+            if distance > dist:
+                dist = distance
+                index_val = i
+    return index_val
 
 def Optimal(frames, pages):
-        f = [-1]*frames
-        hits = 0
-        miss = 0
-        for i, page in enumerate(pages):
-            if page in f:
-                hits += 1
+    f = [-1] * frames
+    hits = 0
+    miss = 0
+    for i, page in enumerate(pages):
+        if page in f:
+            hits += 1
+        else:
+            miss += 1
+            if -1 in f:
+                index_val = f.index(-1)
+                f[index_val] = page
             else:
-                miss += 1
-                if -1 in f:
-                    index = f.index(-1)
-                    f[index] = page
-                else:
-                    replace_index = farthest_index(f,pages,i)
-                    f[replace_index] = page
-            print(f"Frames:{f}")
-        return hits, miss
+                replace_index = index(f, pages, i)
+                f[replace_index] = page
+        print(f"Frames: {f}")
+    return hits, miss
+
+
+frames = int(input("Enter the number of frames: "))
+pages = list(map(int, input("Enter the pages (space-separated): ").split()))
+
+hits, miss = Optimal(frames, pages)
+print("Hits:", hits)
+print("Misses:", miss)
